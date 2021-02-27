67fc6afa413e57483c58dc312f10259ef1c4687a
Nitin Pradeep Kumar <getnpk@gmail.com>
2021-02-27 12:01:41 +0900

    Create validAnagram.js

---
+++ easy/strings/validAnagram.js
@@ -0,0 +1,32 @@
+/**
+ * https://leetcode.com/problems/valid-anagram/
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+    
+    const map = {}
+    
+    for (let i = 0; i < s.length; i++) {
+        if(map[s[i]]) {
+            map[s[i]] += 1
+        } else {
+            map[s[i]] = 1
+        }
+    }
+    
+    for (let i = 0; i < t.length; i++) {
+        if (map[t[i]]) {
+            map[t[i]] -= 1;
+            if (map[t[i]] === 0) {
+                delete map[t[i]]
+            }
+        } else {
+            return false;
+        }
+    }
+    
+    return Object.keys(map).length === 0
+};
