2e45f900aa64999d87ce15421bf5c07222e32452
Nitin Pradeep Kumar <getnpk@gmail.com>
2021-02-28 08:58:42 +0900

    Create firstBadVersion.js

---
+++ easy/sortingAndSearching/firstBadVersion.js
@@ -0,0 +1,36 @@
+/**
+ * https://leetcode.com/problems/first-bad-version/
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let l = 0
+        let r = n - 1;
+        
+        while (l <= r) {
+            let mid = Math.floor((l + r) / 2)
+            if (isBadVersion(mid)) {
+                r = mid - 1;
+            } else {
+                l = mid + 1
+            }
+        }
+        
+        return l
+    };
+};
