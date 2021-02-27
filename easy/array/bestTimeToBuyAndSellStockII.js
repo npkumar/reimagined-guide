a3cd92330f2a5daf70e5741e5c96d83bbc3c8545
Nitin Pradeep Kumar <getnpk@gmail.com>
2021-02-27 10:34:38 +0900

    Create bestTimeToBuyAndSellStockII.js

---
+++ easy/array/bestTimeToBuyAndSellStockII.js
@@ -0,0 +1,16 @@
+/**
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let max = 0;
+    
+    for (let i = 0; i < prices.length - 1; i++) {
+        if (prices[i + 1] > prices[i]) {
+            max += prices[i + 1] - prices[i]
+        }
+    }
+    
+    return max;
+};
