360983caa0d3f21496dc652ac8ef25f4c0867e4f
Nitin Pradeep Kumar <getnpk@gmail.com>
2021-03-03 10:40:35 +0900

    Create binaryTreeInorderTraversal.js

---
+++ medium/treesAndGraphs/binaryTreeInorderTraversal.js
@@ -0,0 +1,25 @@
+/**
+ * https://leetcode.com/problems/binary-tree-inorder-traversal/
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    const result = []
+    const inOrder = (root) => {
+        if (root === null) return
+        inOrder(root.left)
+        result.push(root.val)
+        inOrder(root.right)
+    }
+    
+    inOrder(root)
+    return result;
+};
