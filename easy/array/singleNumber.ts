e416275499eaf94eac50086e460fe9625157d351
Nitin Pradeep Kumar <getnpk@gmail.com>
2022-03-29 12:24:18 +0900

    Create singleNumber.ts

---
+++ easy/array/singleNumber.ts
@@ -0,0 +1,16 @@
+/**
+* https://leetcode.com/problems/single-number
+*/
+function singleNumber(nums: number[]): number {
+    const set = new Set<number>();
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (set.has(nums[i])) {
+            set.delete(nums[i])
+        } else {
+            set.add(nums[i])
+        }
+    }
+    
+    return [...set][0];
+};
